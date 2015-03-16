508691c7f6854a8c911e1d49c88136e4d59214d6
Bloblblobl <Bloblblobl@gmail.com>
2015-03-15 19:59:10 -0700

    Added login/register on startup

--- cli.py
+++ cli.py
@@ -4,11 +4,13 @@ If you provide a username and password, it will log you into the server
 If you provide a handle as well, it will register you to the server instead
 """
 import argparse
+from rumble_client.client import Client
 
 def options():
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('--user', required=True)
     parser.add_argument('--password', required=True)
+    parser.add_argument('--server-url', default='http://rumble.pythonanywhere.com')
     parser.add_argument('--handle')
 
     return parser.parse_args()
@@ -18,6 +20,15 @@ def main():
     print o.user
     print o.password
     print o.handle
+    client = Client(o.server_url)
+    if o.handle is not None:
+        client.register(o.user, o.password, o.handle)
+    user = client.login(o.user, o.password)
+    while True:
+        user_input = raw_input('> ')
+        print user_input
+
+
 
 if __name__ == '__main__':
     main()
