2971e72a9c6a409424bc1fde50ae028fdad8c5ff
Bloblblobl <Bloblblobl@gmail.com>
2015-04-05 14:22:11 -0700

    Displayed messages properly

--- cli.py
+++ cli.py
@@ -4,6 +4,9 @@ If you provide a username and password, it will log you into the server
 If you provide a handle as well, it will register you to the server instead
 """
 import argparse
+import time
+import os
+from datetime import datetime, timedelta
 from rumble_client.client import Client
 
 def options():
@@ -24,10 +27,30 @@ def main():
     if o.handle is not None:
         client.register(o.user, o.password, o.handle)
     user = client.login(o.user, o.password)
-    while True:
-        user_input = raw_input('> ')
-        print user_input
 
+    #select room
+    print client.get_rooms()
+    room = 'room0'
+    time = datetime.utcnow().replace(microsecond=0)
+    start = time - timedelta(days=1)
+    end = time + timedelta(days=1)
+    print client.join_room(room)
+
+    while True:
+        print '==============LINE=BREAK=============='
+        user_input = raw_input('> ')
+        parse_input(user_input, client, room)
+        # clear the screen
+        messages = client.get_messages(room, start, end)
+        for m in messages["result"]:
+            print "[{}:{}] {}".format(m[1], m[0], m[2])
+
+def parse_input(input, client, room):
+    # check to see if input is command
+    if input.startswith('!'):
+        return
+    # if not treat it as a message, send the message
+    print client.send_message(room, input)
 
 
 if __name__ == '__main__':
