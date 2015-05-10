75baef8eb9394aea3de3a68483c67c4995734c4c
Bloblblobl <Bloblblobl@gmail.com>
2015-05-10 16:35:37 -0700

    Added config file

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Rumble-CLI
 Command Line Interface for [Rumble-Server](https://github.com/Bloblblobl/Rumble-Server)
+Server URL: http://rumble.pythonanywhere.com
 
+## Commands
+* q: Exit
+* help: Display the help prompt
+* register: Register a new account on a server
+* login: Login to the a server
+* join: Join a different room
+* gtrms: Get the rooms on the server
+* gtmmbrs: Get the members of the current room
+* create: Create a new room and join it
--- cli.py
+++ cli.py
@@ -4,12 +4,18 @@ If you provide a username and password, it will log you into the server
 If you provide a handle as well, it will register you to the server instead
 """
 import argparse
+import json
 import time
 import os
+import sys
 from datetime import datetime, timedelta
 from rumble_client.client import Client
 
+
+
+
 def options():
+    # --user a --password a --server-url http://localhost:5555
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('--user', required=True)
     parser.add_argument('--password', required=True)
@@ -18,15 +24,37 @@ def options():
 
     return parser.parse_args()
 
-def main():
-    o = options()
-    print o.user
-    print o.password
-    print o.handle
-    client = Client(o.server_url)
-    if o.handle is not None:
-        client.register(o.user, o.password, o.handle)
-    user = client.login(o.user, o.password)
+def startup():
+    if os.path.isfile('config.json'):
+        config = open('config.json').read()
+        config = json.loads(config)
+        return config['user'], config['password'], config['handle'], config['server_url']
+
+    print 'Rumble-CLI'
+    user = raw_input('Username: ')
+    password = raw_input('Password: ')
+    handle = raw_input('Handle: ')
+    server_url = raw_input('Server URL: ')
+
+    s = json.dumps(dict(user=user, password=password, handle=handle, server_url=server_url))
+    open('config.json', 'w').write(s)
+
+    return user, password, handle, server_url
+
+
+def main(username, password, handle, server_url):
+    client = Client(server_url)
+    commands = {'q': sys.exit,
+                'help': help,
+                'register': client.register,
+                'login': client.login,
+                'join': client.join_room,
+                'gtrms': client.get_rooms,
+                'gtmmbrs': client.get_room_members,
+                'create': client.create_room}
+    if handle:
+        client.register(username, password, handle)
+    user = client.login(username, password)
 
     #select room
     print client.get_rooms()
@@ -39,19 +67,29 @@ def main():
     while True:
         print '==============LINE=BREAK=============='
         user_input = raw_input('> ')
-        parse_input(user_input, client, room)
-        # clear the screen
+        parse_input(user_input, client, room, commands)
         messages = client.get_messages(room, start, end)
+        os.system('cls')
         for m in messages["result"]:
             print "[{}:{}] {}".format(m[1], m[0], m[2])
 
-def parse_input(input, client, room):
+def parse_input(input, client, room, commands):
     # check to see if input is command
     if input.startswith('!'):
-        return
+        args = input[1:].split(' ')
+        command = args[0]
+        args = args[1:]
+        commands[command](*args)
     # if not treat it as a message, send the message
-    print client.send_message(room, input)
+    else:
+        print client.send_message(room, input)
 
+def help():
+    help_file = open('README.md')
+    for line in help_file:
+        print(line)
+    raw_input('PRESS ANY KEY TO CONTINUE')
 
 if __name__ == '__main__':
-    main()
+    user, password, handle, server_url = startup()
+    main(user, password, handle, server_url)
